cded9db62495582f45d59017bf18e94e8d51b7df
likewhat9901 <parkko123@naver.com>
2025-07-15 17:56:36 +0900

    git

--- src/main/java/common/JDBConnect.java
+++ src/main/java/common/JDBConnect.java
@@ -1,6 +1,8 @@
 package common;
 
 import java.sql.*;
+
+import jakarta.servlet.ServletContext;
 //import java.sql.Connection;
 //import java.sql.DriverManager;
 //import java.sql.PreparedStatement;
@@ -11,7 +13,7 @@ import java.sql.*;
 public class JDBConnect {
 	
 	// 멤버변수 : JDBC 프로그래밍을 위한 객체
-    public Connection conn = null;
+    public Connection con = null;
     public Statement stmt;
     public PreparedStatement psmt = null;
     public ResultSet rs = null;
@@ -19,13 +21,13 @@ public class JDBConnect {
     public JDBConnect() {
     	try {
     		// 오라클 드라이버 로드
-            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Class.forName("oracle.jdbc.OracleDriver");
             // 커넥션URL, 아이디, 비번
             String url = "jdbc:oracle:thin:@localhost:1521/xe";
             String id = "musthave";
             String pwd = "1234";
             // 연결 시도 및 Connection 객체 반환
-            conn = DriverManager.getConnection(url, id, pwd);
+            con = DriverManager.getConnection(url, id, pwd);
             
             System.out.println("DB 연결 성공(기본 생성자)");
     	}
@@ -40,7 +42,7 @@ public class JDBConnect {
             if (rs != null) rs.close();
             if (stmt != null) stmt.close();
             if (psmt != null) psmt.close();
-            if (conn != null) conn.close();
+            if (con != null) con.close();
             
             System.out.println("JDBC 자원 해제");
     	}
@@ -49,4 +51,37 @@ public class JDBConnect {
     	}
     }
     
+    //인수생성자 : 4개의 매개변수 선언
+    public JDBConnect(String driver, String url, String id, String pwd) {
+        try {
+           Class.forName("oracle.jdbc.OracleDriver");
+           con = DriverManager.getConnection(url, id, pwd);
+           System.out.println("DB 연결 성공(인수 생성자1)");
+        }
+        catch (Exception e) {
+           e.printStackTrace();
+        }
+     }
+    
+    //인수생성자2 : application 내장객체를 매개변수로 선언
+    public JDBConnect(ServletContext application) {
+	    //JSP에서 전달해준 내장객체를 받은 후 사용한다.
+    	try {
+    		/*
+    		 * 내장객체를 전달받았으므로 Java클래스에서 web.xml에 접근할 수 있다.
+    		 * 그러면 JSP에서 DB연결을 위해 반복적으로 사용했던 코드를 아래와 같이
+    		 * 정의하여 중복된 코드를 줄일 수 있다.
+    		 */
+	       String driver = application.getInitParameter("OracleDriver");			
+		   Class.forName(driver);
+		   String url = application.getInitParameter("OracleURL");
+		   String id = application.getInitParameter("OracleId");
+		   String pwd = application.getInitParameter("OraclePwd");
+		   con = DriverManager.getConnection(url, id, pwd);
+		   System.out.println("DB 연결 성공(인수 생성자2)");
+	    }
+	    catch (Exception e) {
+	       e.printStackTrace();
+	    }
+	}
 }
